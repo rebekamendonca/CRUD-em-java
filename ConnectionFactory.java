a7d4f1d59b244f55f75c0acc6454b2be101cabe0
Rebeka Mendonça <mendonca.rebeka.n@gmail.com>
2017-12-27 22:14:11 -0300

    Add files via upload

---
+++ ConnectionFactory.java
@@ -0,0 +1,43 @@
+package control;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.postgresql.jdbc.PgResultSet;
+import org.postgresql.jdbc.PgStatement;
+public class ConnectionFactory {
+	//Eles estao como static porque nao quero instanciar para pegar o metodo so quero charmar
+
+	private final static String DRIVER = "org.postgresql.Driver";
+	private final static String URL = "jdbc:mysql://localhost:5432/dbmercadinho";
+	private final static String USER = "pgsql";
+	private final static String PASS = "";
+	
+//Criando uma conexão
+	public static Connection getConnection() {
+		try {
+			
+			Class.forName(DRIVER); 
+			return DriverManager.getConnection(URL, USER, PASS);
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException("Erro na conexão", e);
+		}
+
+	}
+	
+//fechando  a conexão com o banco
+	public static void closeConnection(Connection con,PgStatement stmt,PgResultSet rs){
+		closeConnection(con,stmt,rs);
+			try {
+				if(rs != null){
+				((Connection) rs).close();
+				}
+			}catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new RuntimeException("Erro no fechamento da conexão com o banco de dados", e);
+			}
+			
+		}
+	}
+
